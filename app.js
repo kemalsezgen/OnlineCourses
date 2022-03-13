2b1edb2649c471cee38a225b79ba88d668473c47
Kemal Sezgen <kemalsezgen1@gmail.com>
2022-03-14 01:28:27 +0300

    yonlendirmeler yapildi, html -> ejs donusumu tamamlandı, partialler olusturuldu

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+  res.status(200).render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about');
+});
+app.get('/course', (req, res) => {
+  res.status(200).render('course');
+});
+app.get('/teacher', (req, res) => {
+  res.status(200).render('teacher');
+});
+app.get('/contact', (req, res) => {
+  res.status(200).render('contact');
 });
 
 const port = 3000;
